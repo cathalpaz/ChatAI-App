17af3567221da20c6e99b6f82a05789423d8b729
Cathal <cathalpaz@gmail.com>
2023-05-21 00:51:21 -0400

    components + sockets added

---
+++ client/src/components/chat/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from "react-chat-engine-advanced"
+
+const Chat = () => {
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    "testuser", // username
+    "1234" // password
+  )
+  return <div style={{ flexBasis: "100%"}}>
+    <MultiChatSocket {...chatProps} />
+    <MultiChatWindow
+      {...chatProps}
+      style={{ height: "100vh"}}
+      renderChatHeader={(chat) => <Header chat={chat} />}
+    />
+  </div>;
+
+}
+
+export default Chat;
